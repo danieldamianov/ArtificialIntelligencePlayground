120f56dd5ab9a129dfbf340e13d362f960715551
danieldamianov <danieldamianov02@gmail.com>
2020-05-26 14:38:00 +0300

    Added a CarsPricePrediction.csproj.

---
+++ ML.NET/Regression/CarsPricePrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarsPricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
